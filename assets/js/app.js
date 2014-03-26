3fe5f680fc0544c2ddb8f1c9fdbea4a043998eab
mknadler <miriam.k.nadler@gmail.com>
2014-03-26 19:27:43 -0400

    New sun style + background color

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+(function () {
+var path = document.querySelector('svg circle');
+var length = path.getTotalLength();
+// Clear any previous transition
+path.style.transition = path.style.WebkitTransition =
+  'none';
+// Set up the starting positions
+path.style.strokeDasharray = length + ' ' + length;
+path.style.strokeDashoffset = length;
+// Trigger a layout so styles are calculated & the browser
+// picks up the starting position before animating
+path.getBoundingClientRect();
+// Define our transition
+path.style.transition = path.style.WebkitTransition =
+  'stroke-dashoffset 2s ease-in-out';
+// Go!
+path.style.strokeDashoffset = '0';
+});
