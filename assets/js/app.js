624bcbb0519ac9488c08630cf9c3ce93e58d66e3
mknadler <miriam.k.nadler@gmail.com>
2014-03-26 23:07:50 -0400

    Functional!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,41 @@
-(function () {
-var path = document.querySelector('svg circle');
-var length = path.getTotalLength();
-// Clear any previous transition
-path.style.transition = path.style.WebkitTransition =
-  'none';
-// Set up the starting positions
-path.style.strokeDasharray = length + ' ' + length;
-path.style.strokeDashoffset = length;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-path.getBoundingClientRect();
-// Define our transition
-path.style.transition = path.style.WebkitTransition =
-  'stroke-dashoffset 2s ease-in-out';
-// Go!
-path.style.strokeDashoffset = '0';
+$( document ).ready(function() {
+  $("#howlong").css("display", "none");
 });
+
+
+var z=document.getElementById("howlong");
+
+function sunsetCalc(){
+  getSunset();
+  getLocation();
+}
+
+function getSunset()
+  {
+    if (navigator.geolocation)
+      {navigator.geolocation.getCurrentPosition(showSunset);}
+    else
+      {y.innerHTML="Geolocation is not supported by this browser.";}
+  }  
+function showSunset(position)
+      {
+    var la = position.coords.latitude;
+    var lo = position.coords.longitude;
+    var now = new Date();
+    var when = SunCalc.getTimes(new Date(), la, lo);
+    var then = when.sunset;
+    var difference = then - now;
+    z.innerHTML = difference;
+    var seconds = Math.floor(difference / 1000);
+    var numhours = Math.floor(seconds / 3600);
+    var numminutes = Math.floor((seconds % 3600) / 60);
+    var numseconds = Math.floor((seconds % 3600) % 60);
+    if (difference < 0) {
+      numhours = numhours * -1;
+      numminutes = numminutes * -1;
+      numseconds = numseconds * -1;
+      z.innerHTML = numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds ago!";
+      tooLate();
+    }
+   
+}
