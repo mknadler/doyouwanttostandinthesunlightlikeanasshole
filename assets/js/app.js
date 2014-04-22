4367bd511a1aa9928dcb58ba3b488ff96255c823
Miriam Nadler <miriam.k.nadler@gmail.com>
2014-04-22 13:44:49 -0400

    Refactored JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,75 @@
-afterDisplay = null;
-nightTime = null;
-dayTime = null;
-afterDisplay = function (){
-   $(".time").css("display", "inline-block");
-}
-$(function() {
-  $(".cloud").addClass("animate-cloudToLeft");
-/*
-  function afterDisp() {
-    $(".time").css("display", "inline-block");
-  }
-  afterDisplay = afterDisp;
-*/
-  function isNight() {
-    $(".svg-container").addClass("moon");
-    $("html").addClass("moon");
-    $(".about span, #howlong").css("color", "#DDD");
-    $(".about span").css("opacity", "0");
-    $("#howlong").css("opacity", "1");
-  }
-  nightTime = isNight;
-  function isDay() {
-    $(".svg-container").addClass("day");
-  }
-  dayTime = isDay;
+// Declaring variables at global scope so that they can be used througout the script 
+var about = $(".about span");
+var output = $("#howlong");
+var sun = $(".sun-selector");
+
+// Prime everything.
+
+$(function(){
+	$(".cloud").addClass("animate-cloudToLeft");
+	sun.click(geolocate);
 });
-var z=document.getElementById("howlong");
-function sunsetCalc(){
-  getSunset();
+
+// Grab geolocation data & pass the Position object
+// created by the geolocation API to the sunsetCalc function
+
+var geolocate = function(isfirst) {	
+	console.log("geolocate");
+	if ($("body").hasClass("initial")) {
+		about.text("Calculating...");
+		$("body").removeClass("initial");
+	}
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(sunsetCalc);
+	} else {
+		alert("Sorry! Geolocation is not supported by this browser. We will be adding zip code functionality soon.")
+	}
 }
 
-function getSunset()
-  {
-    if (navigator.geolocation)
-      {navigator.geolocation.getCurrentPosition(showSunset);}
-    else
-      {y.innerHTML="Geolocation is not supported by this browser.";}
-  }  
-function showSunset(position)
-      {
-    var la = position.coords.latitude;
-    var lo = position.coords.longitude;
-    var now = new Date();
-    var when = SunCalc.getTimes(new Date(), la, lo);
-    var then = when.sunset;
-    var gmorningtoya = when.sunrise;
-    var difference = then - now;
+var sunsetCalc = function(position){
+	console.log("sunsetcalc");
+	// pull info from the Position object created by
+	// the geolocation; run them through Sun Calc;
+	// set up the variables we'll need later.
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var now = new Date();
+	var all_times = SunCalc.getTimes(new Date(), latitude, longitude);
+	var sunset_time = all_times.sunset;
+	var sunrise_time = all_times.sunrise;
+	
+	// figure out the difference between
+	// now and sunset 
+	// & convert it to hours, minutes, and seconds
+	var difference = sunset_time - now;
     var seconds = Math.floor(difference / 1000);
     var numhours = Math.floor(seconds / 3600);
     var numminutes = Math.floor((seconds % 3600) / 60);
     var numseconds = Math.floor((seconds % 3600) % 60);
-    if (gmorningtoya > now) {
-      z.innerHTML = "It isn't even sunrise yet, calm down!";
-      nightTime();
+    
+    // Output the time + change css classes
+    // depending on whether it is daytime or nighttime
+    if (sunrise_time > now) {
+    	output.text("It isn't even sunrise yet!");
+    } else if (difference < 0) {
+    	numhours = numhours * -1;
+      	numminutes = numminutes * -1;
+      	numseconds = numseconds * -1;
+      	output.text("Oh no! The sun set " + numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds ago!");	
+    	$(".svg-container").addClass("moon");
+    	$("html").addClass("moon");
+    	$(".about span, #howlong").css("color", "#DDD");
+    } else if (numhours <=2 && numhours >= 1) {
+    	output.text("Better hurry! You have " + numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds of sunlight left!");
+    	$(".svg-container").addClass("day");
+    } else if (numhours < 1) {
+    	output.text("Better hurry! You have " + numminutes + " minutes, and " + numseconds + " seconds of sunlight left!");	
+    	$(".svg-container").addClass("day");
+    } else if (numhours >= 2) {
+    	output.text("You have " + numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds of sunlight left!");
+    	$(".svg-container").addClass("day");
     }
-    else if (difference < 0) {
-      numhours = numhours * -1;
-      numminutes = numminutes * -1;
-      numseconds = numseconds * -1;
-      z.innerHTML = "Oh no! The sun set " + numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds ago!";
-      nightTime();
-    }
-    else if (numhours <=2 && numhours >= 1) {
-      z.innerHTML = "Better hurry! You have " + numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds of sunlight left!";
-      dayTime();
-    }
-    else if (numhours < 1) {
-      z.innerHTML = "Better hurry! You have " + numminutes + " minutes, and " + numseconds + " seconds of sunlight left!";
-      dayTime();
-    }
-    else if (numhours >=2) {
-      z.innerHTML = "You have " + numhours + " hours, " + numminutes + " minutes, and " + numseconds + " seconds of sunlight left!";
-      dayTime();
-    }
-    afterDisplay();
+    // Hide the top text, show the bottom text
+    about.empty();
+    $(".time").css("display", "inline-block");
 }
